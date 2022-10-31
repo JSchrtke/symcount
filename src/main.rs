1df1cabd8b9858b87b7b6a5da8021ab744a06276
jschrtke <jschrtke@protonmail.com>
2022-10-31 18:57:56 +0100

    Count symbols in all files

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-use std::fs::File;
+use std::collections::HashMap;
+use std::fs::{self, File};
 use std::io::Read;
-use std::path::Path;
+use std::path::PathBuf;
 
-const symbols = [";"];
+const SYMBOLS: [char; 29] = [
+    '.', ',', '<', '>', '?', '/', '!', '"', '@', '$', '%', '\'', '(', ')', '|', '{', '}', '^', '&',
+    '*', '~', '-', '[', ']', '#', '=', '+', ':', '\\',
+];
 
-fn main() {
-    let filepath = Path::new("./test.txt");
-    let mut file: File = match File::open(&filepath) {
-        Err(err) => panic!("Error opening file '{}': {}", filepath.display(), err),
-        Ok(f) => f,
-    };
-
-    let mut buf = String::new();
-    file.read_to_string(&mut buf)
-        .unwrap_or_else(|_| panic!("Error reading file '{}':", filepath.display()));
-
-    for char in buf.chars() {
-        if is_symbol(char) {
-            println!("{}", char);
+fn main() -> std::io::Result<()> {
+    let dir = fs::read_dir(".")?;
+    let mut file_paths: Vec<PathBuf> = Vec::new();
+    for entry in dir {
+        let entry = entry?;
+        if entry.path().is_file() {
+            file_paths.push(entry.path())
         }
     }
+
+    let mut symbol_counts: HashMap<char, usize> = HashMap::new();
+    for path in file_paths {
+        let mut file = File::open(&path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        content.chars().filter(is_symbol).for_each(|symbol| {
+            symbol_counts
+                .entry(symbol)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        });
+    }
+
+    symbol_counts
+        .iter()
+        .for_each(|count| println!("{}: {}", count.0, count.1));
+
+    Ok(())
 }
 
-fn is_symbol(c: char) -> bool {
-    false
+fn is_symbol(c: &char) -> bool {
+    SYMBOLS.contains(c)
 }
